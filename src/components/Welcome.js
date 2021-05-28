fb3b6f1babf7fa245d5802598ffde4a559f9f1b5
Vu Nguyen <nguyentranvu0104@gmail.com>
2021-05-28 13:33:17 -0400

    Added JOSN to represent data and made component dynamic.

---
+++ src/components/Welcome.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import imageLinksData from './data/image_links.json'
+
+const Welcome = () => {
+  return (
+    <div className="scene" id="welcome">
+      <article className="content">
+        <div className="gallery">
+          {
+            imageLinksData.map((link) => 
+              <img className={link.class} src={link.src} alt={link.alt} />
+            )
+          }
+        </div>
+        <h1>Welcome to the Landon&nbsp;Hotel</h1>
+        <p>The original Landon perseveres after 50 years in the heart of West London. The West End neighborhood has something for everyone—from theater to dining to historic sights. And the not-to-miss Rooftop Cafe is a great place for travelers and locals to engage over drinks, food, and good&nbsp;conversation. &nbsp;To learn more about the Landon Hotel in the West End, browse our website and <a href="files/landon_information_sheet_London.pdf">download our handy information sheet</a>.</p>
+      </article>
+    </div>
+  );
+}
+
+export default Welcome;
